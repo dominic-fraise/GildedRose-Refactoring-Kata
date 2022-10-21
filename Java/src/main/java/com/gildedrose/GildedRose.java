f3b092c7d34bfff0712cf5df3edf3838eba19084
iliaPetrovs <ilja.petrovs@transferwise.com>
2022-10-21 15:36:38 +0100

    introduce constant for backstage passes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,6 +8,7 @@ package com.gildedrose;
 class GildedRose {
     public static final int MAX_QUALITY = 50;
     public static final int MIN_QUALITY = 0;
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -17,7 +18,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie" )
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    && !items[i].name.equals(BACKSTAGE_PASSES)) {
                 if (items[i].quality > MIN_QUALITY && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                     items[i].quality = items[i].quality - 1;
                 }
@@ -25,7 +26,7 @@ class GildedRose {
                 if (items[i].quality < MAX_QUALITY) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (items[i].name.equals(BACKSTAGE_PASSES)) {
                         increaseBackstagePassQuality(i);
                     }
                 }
@@ -37,7 +38,7 @@ class GildedRose {
 
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!items[i].name.equals(BACKSTAGE_PASSES)) {
                         if (items[i].quality > MIN_QUALITY) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
