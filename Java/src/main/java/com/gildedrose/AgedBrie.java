1cc9a82cd6bdb2b55fdd302d93f12987500fe2e9
Dominic Fraise <dominic.fraise@transferwise.com>
2023-01-13 15:50:36 +0000

    Remove super class methods

--- Java/src/main/java/com/gildedrose/AgedBrie.java
+++ Java/src/main/java/com/gildedrose/AgedBrie.java
@@ -26,28 +26,4 @@ public class AgedBrie extends Item {
             increaseQuality();
         }
     }
-
-    public boolean nameIs(String itemName) {
-        return name.equals(itemName);
-    }
-
-    public boolean qualityCanBeIncreased() {
-        return quality < GildedRose.MAX_QUALITY;
-    }
-
-    public int increaseQuality() {
-        return quality++;
-    }
-
-    public boolean qualityCanBeReduced() {
-        return quality > GildedRose.MIN_QUALITY;
-    }
-
-    public boolean nameIsNot(String itemName) {
-        return !nameIs(itemName);
-    }
-
-    public int decreaseQuality() {
-        return quality--;
-    }
 }
